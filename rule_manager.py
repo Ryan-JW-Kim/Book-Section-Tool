f2c5af621ed902d8e2b05071c18382637bb17caa
Ryan Kim <43964171+Ryan-JW-Kim@users.noreply.github.com>
2022-05-18 13:38:59 -0400

    Update rule_manager.py

--- rule_manager.py
+++ rule_manager.py
@@ -1,6 +1,13 @@
+import Document_manager
+
 
 class Rule_manager:
   
-    def __init__(self, document_object):
+    def __init__(self):
       
       self.doc = document_object
+      self.proposed_changes =  
+    def apply_rules(self, body):
+      pass
+    
+    
